3c60094300b5290970dcbcccfca61896e3672717
Sebastian Campos <seabasscampos@gmail.com>
2017-01-11 01:48:01 +0000

    Add files via upload

---
+++ reverse_powershell_payload.rb
@@ -0,0 +1,11 @@
+#!/bin/ruby
+
+system("ruby reverse_powershell_payload.rb")
+system("mv powershell_reverse_ducky.txt reverse_powershell_payload.txt")
+system("tail -n+7 reverse_powershell_payload.txt > reverse_powershell_payload.tmp")
+system("rm reverse_powershell_payload.txt")
+system("mv reverse_powershell_payload.tmp reverse_powershell_payload.txt")
+system("sed 's/^.......//' reverse_powershell_payload.txt > reverse_powershell_payload.tmp")
+system("sed -i '$ d' reverse_powershell_payload.tmp")
+system("mv reverse_powershell_payload.tmp reverse_powershell_payload.bat")
+system("rm reverse_powershell_payload.txt")
